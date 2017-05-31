645203be651358839da74c192cb88dcc996a8d5d
Corey Moten <clmoten@gmail.com>
2017-05-31 19:23:17 -0400

    Initial configuration service commit

---
+++ src/main/java/io/gearstack/configsvc/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package io.gearstack.configsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * ConfigServerApplication
+ *
+ * Configuration server application class
+ */
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
